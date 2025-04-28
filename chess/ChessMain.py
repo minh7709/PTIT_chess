1185f7832c7f4ef9ae0a0ffaf79237045864d539
minh7709 <minh2k32003@gmail.com>
2025-04-28 19:06:25 +0700

    Sua doi animateMove

--- chess/ChessMain.py
+++ chess/ChessMain.py
@@ -264,7 +264,7 @@ def animateMove(move, screen, board, clock):
     global colors
     d_row = move.end_row - move.start_row
     d_col = move.end_col - move.start_col
-    frames_per_square = 10  # frames to move one square
+    frames_per_square = 3  # frames to move one square
     frame_count = (abs(d_row) + abs(d_col)) * frames_per_square
     for frame in range(frame_count + 1):
         row, col = (move.start_row + d_row * frame / frame_count, move.start_col + d_col * frame / frame_count)
